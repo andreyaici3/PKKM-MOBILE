63398c8e322ff2b0e750473909f071c6a0c3c041
andreyaici3 <andreyandri90@gmail.com>
2022-11-21 14:04:42 +0700

    add db

---
+++ Tugas1_PKKM/app/src/main/java/com/andrey/tugas1_pkkm/DBUser.java
@@ -0,0 +1,82 @@
+package com.andrey.tugas1_pkkm;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+public class DBUser extends SQLiteAssetHelper {
+
+    private static final String DATABASE_NAME = "my_first_db.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String NAMA_TABEL = "pengguna";
+    private static final String KEY_ID="id_pengguna";
+    private static final String KEY_USERNAME="username";
+
+    public DBUser(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public boolean isNull(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String count = "SELECT count(*) FROM "+ NAMA_TABEL +"";
+        Cursor mcursor = db.rawQuery(count, null);
+        mcursor.moveToFirst();
+        int icount = mcursor.getInt(0);
+        db.close();
+        return icount <= 0;
+    }
+
+    public void selct(){
+        String countQuery = "SELECT  * FROM " + NAMA_TABEL;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        int rowCount = cursor.getCount();
+        db.close();
+        cursor.close();
+
+
+    }
+
+    public User findUser()
+    {
+        SQLiteDatabase db=this.getReadableDatabase();
+        Cursor cursor=db.query(NAMA_TABEL,new  String[]{KEY_ID,KEY_USERNAME},null,null,null,null,null);
+        User u = new User();
+        if (cursor!=null && cursor.moveToFirst())
+        {
+            cursor.moveToFirst();
+            u.setUserId(cursor.getInt(cursor.getColumnIndex("id_pengguna")));
+            u.setUsername(cursor.getString(cursor.getColumnIndex("username")));
+        }else
+        {
+            u.setUserId(0);
+            u.setUsername("");
+        }
+        db.close();
+
+        Log.d("hmzz", u.getUsername());
+        return u;
+    }
+
+    public void delete()
+    {
+        SQLiteDatabase db=this.getWritableDatabase();
+        db.delete(NAMA_TABEL, null, null);
+        db.close();
+    }
+
+    public void update(User user){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put(KEY_ID,user.getUserId());
+        values.put(KEY_USERNAME,user.getUsername());
+        db.update(NAMA_TABEL, values, KEY_ID + " = ?",
+                new String[] { String.valueOf(user.getUserId())
+                });
+        db.close();
+    }
+}
--- Tugas1_PKKM/app/src/main/java/com/andrey/tugas1_pkkm/MainActivity.java
+++ Tugas1_PKKM/app/src/main/java/com/andrey/tugas1_pkkm/MainActivity.java
@@ -3,9 +3,11 @@ package com.andrey.tugas1_pkkm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,16 +16,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText etNim = (EditText) findViewById(R.id.etNim);
-        EditText etPassword = (EditText) findViewById(R.id.etPassword);
-
+        EditText etId = (EditText) findViewById(R.id.etId);
+        EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        TextView status = (TextView) findViewById(R.id.statusLogin);
         Button btnLogin = (Button) findViewById(R.id.btnLogin);
+
+
+
+
+
+
+        DBUser dbUser = new DBUser(this);
+
+
+        dbUser.selct();
+
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
+                User u = dbUser.findUser();
+
+                if (String.valueOf(u.getUserId()).equals(etId.getText().toString()) && u.getUsername().equals(etUsername.getText().toString())){
+                    status.setText("Login Berhasil");
+                } else {
+                    status.setText("Login Gagal");
+                }
             }
         });
+
+
     }
 }
